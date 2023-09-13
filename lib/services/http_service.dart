8f65827d6831e598528584e12d047da5b7ca9ee9
Olaoluwaalo <olaoluwagabriel7@gmail.com>
2023-09-13 13:40:44 -0700

    step 3

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -1,15 +1,44 @@
 import 'package:dio/dio.dart';
+import 'dart:developer' show log;
 
 class HttpService {
-  Dio dio;
+  //get an instance of a Dio add from pub.dev
+  final _dio = Dio(
+    BaseOptions(
+      baseUrl: "https://reqres.in/",
+    ),
+  );
+  HttpService();
 
-  final baseUrl = "https://reqres.in/";
+//create a function to getRequest
 
-  HttpService() {
-    dio = Dio(
-      BaseOptions(
-        baseUrl: baseUrl,
-      ),
-    );
+  Future<Response> getRequest(endpoint) async {
+    Response response;
+    try {
+      response = await _dio.get(endpoint);
+      // ignore: deprecated_member_use
+    } on DioError catch (e) {
+      log(e.message.toString());
+      throw Exception();
+    }
+    return response;
+  }
+
+//Allows to get interceptors
+  void interceptors() {
+    _dio.interceptors.add(InterceptorsWrapper(
+      onError: (e, handler) {
+        log(e.message.toString());
+        //This communicates the error encountered while consuming Api
+      },
+      onRequest: (options, handler) {
+        log(options.data());
+        // Listen to changes while making a request
+      },
+      onResponse: (e, handler) {
+        log(e.data.toString());
+        // Listens to response gotten from Api call
+      },
+    ));
   }
 }
